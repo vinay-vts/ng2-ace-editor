047cc94bc5508126dc75e2aead14378ad4ffc23c
François-xavier Montigny <fxaviermontigny@gmail.com>
2019-01-16 10:21:35 +0100

    [Update] change rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,26 +2,17 @@ import angular from 'rollup-plugin-angular';
 import typescript from 'rollup-plugin-typescript2';
 
 export default {
-    input: 'dist/index.js',
-    output: {
-        file: 'dist/bundles/ng2-ace-editor.umd.js',
-        format: 'umd',
-        name: 'ng.ng2aceeditor',
-        sourcemap: false,
-        globals: {
-            '@angular/core': 'ng.core',
-            '@angular/forms': 'ng.forms'
-        }
-    },
-    external: [
-        '@angular/core',
-        '@angular/forms',
-        'brace',
-        'brace/theme/monokai',
-        'brace/mode/html'
-    ],
-    plugins: [
-        angular(),
-        typescript()
-    ]
-}
+	input: 'dist/index.js',
+	output: {
+		file: 'dist/bundles/ng2-ace-editor.umd.js',
+		format: 'umd',
+		name: 'ng.ng2aceeditor',
+		sourcemap: false,
+		globals: {
+			'@angular/core': 'ng.core',
+			'@angular/forms': 'ng.forms'
+		}
+	},
+	external: ['@angular/core', '@angular/forms', 'brace', 'brace/theme/monokai'],
+	plugins: [angular(), typescript()]
+};
